c4b64437abcb12f2fead85238cfe2f5439755020
Aaron Everette <aeverette0120@gmail.com>
2022-08-14 14:12:27 -0400

    Added the edit form

---
+++ views/places/edit.jsx
@@ -0,0 +1,44 @@
+const React = require("react");
+const Def = require("./default.jsx");
+
+function edit_form(data) {
+  return (
+    <Def>
+      <main>
+        <h1>Edit Place</h1>
+        <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+          
+            <div>
+              <label htmlFor="name">Place Name</label>
+              <input id="name" name="name" required />
+            </div>
+            <div>
+              <label htmlFor="pic">Place Picture</label>
+              <input
+                className="form-control "
+                id="name"
+                name="name"
+                defaultValue={data.place.name}
+              />
+            </div>
+            <div>
+              <label htmlFor="city">City</label>
+              <input id="city" name="city" />
+            </div>
+            <div>
+              <label htmlFor="state">State</label>
+              <input id="state" name="state" />
+            </div>
+            <div>
+              <label htmlFor="cuisines">Cuisines</label>
+              <input id="cuisines" name="cuisines" required />
+            </div>
+            <input type="submit" value="Add Place" />
+          
+        </form>
+      </main>
+    </Def>
+  );
+}
+
+module.exports = edit_form;
