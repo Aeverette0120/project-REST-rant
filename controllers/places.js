627830855ccd12e05c96518de42d79f802415a61
Aaron Everette <aeverette0120@gmail.com>
2022-08-14 11:22:38 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -21,6 +21,18 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {places: places[id] })
+  }
+})
 
 
 // GET /places
